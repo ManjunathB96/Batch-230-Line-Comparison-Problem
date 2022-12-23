da2be4f7d4a62dd2e69b6dac95097d82bec1c74d
Manju31051996 <bbelagavi6@gmail.com>
2022-12-23 19:44:13 +0530

    [Manjunath S B]Day-3 ->LineComparison :added the welcome message

---
+++ src/com/bridgelabz_line_comparison_program/LineComparison.java
@@ -0,0 +1,7 @@
+package com.bridgelabz_line_comparison_program;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Line Comparison Program");
+    }
+}
