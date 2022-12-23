6d759ed8bd42629b1dbd5cc07c13d645dba069cc
Manju31051996 <bbelagavi6@gmail.com>
2022-12-23 20:44:28 +0530

    [Manjunath S B] Day-3 ->LineComparison :UC3 used compareTo() method to compare two lines

--- src/com/bridgelabz_line_comparison_program/LineComparison.java
+++ src/com/bridgelabz_line_comparison_program/LineComparison.java
@@ -4,22 +4,23 @@ public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome To Line Comparison Program");
 
+        // UC1 : create points consisting of cartesian co-ordinates and calculate length
         Line l1 = new Line();
         l1.p1 = new Point();
 
-        l1.p1.x =10;
-        l1.p1.y =20;
+        l1.p1.x = 15;
+        l1.p1.y = 25;
 
         l1.p2 = new Point();
 
-        l1.p2.x =80;
-        l1.p2.y = 100;
+        l1.p2.x = 70;
+        l1.p2.y = 80;
 
         Line l2 = new Line();
 
         l2.p1 = new Point();
 
-        l2.p1.x  = 10;
+        l2.p1.x = 10;
         l2.p1.y = 20;
 
         l2.p2 = new Point();
@@ -34,14 +35,30 @@ public class LineComparison {
         Float length2 = l2.getLength();
         System.out.println(length2);
 
-        if(length1.equals(length2)){
+       // UC-2 : Compare two lines  using equals method
+        if (length1.equals(length2)) {
             System.out.println("Both lines are equal");
 
-        }
-        else {
+        } else {
             System.out.println("lines are not equal");
         }
 
+
+        // UC-3 : Compare two lines Using  compareTo() method
+        Float Value1 = new Float(length1);
+        Float Value2 = new Float(length2);
+
+        int compare = Value1.compareTo(Value2);
+        if (compare > 0) {
+            int sub = (int) (Value1 - Value2);
+            System.out.println(Value1 + " is greater. \nAnswer after subtraction is= " + sub);
+        } else if (compare < 0) {
+            int sub = (int) (Value2 - Value1);
+            System.out.println(Value2 + " is greater. \nAnswer after subtraction is= " + sub);
+        } else {
+            int sub = (int) (Value1 - Value2);
+            System.out.println(" Both are equal! \nAnswer after subtraction is= " + sub);
+        }
 
     }
 }
